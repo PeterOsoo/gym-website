8ce72f3045b2a4cce1d03602b39471f3358180b5
PeterOsoo <posoo617@gmail.com>
2020-11-26 20:29:47 +0300

    Mobile Menu - correct javascript

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const menu = document.querySelector("#mobile-menu")
 const menuLinks = document.querySelector(".navbar__menu")
-const navLogo = document.querySelector(".navbar__logo")
+const navLogo = document.querySelector("#navbar__logo")
 const body = document.querySelector("body")
 
 // display mobile menu
 
 const mobileMenu = () => {
 	menu.classList.toggle("is-active")
-	menuLinks.classList.toggle("is-active")
-	body.classList.toggle("is-active")
+	menuLinks.classList.toggle("active")
+	body.classList.toggle("active")
 }
 
 menu.addEventListener("click", mobileMenu)
