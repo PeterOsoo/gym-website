9af5ae4a7d9955a730f445d0306da943c6bc7e8f
PeterOsoo <posoo617@gmail.com>
2020-12-09 19:35:57 +0300

    animate services section

--- app.js
+++ app.js
@@ -23,3 +23,18 @@ gsap.from(".animate-hero", {
 	y: -150,
 	stagger: 0.3,
 })
+
+gsap.from(".animate-services", {
+	scrollTrigger: ".animate-services",
+	duration: 0.5,
+	opacity: 1,
+	x: -150,
+	stagger: 0.12,
+})
+
+gsap.from(".animate-img", {
+	scrollTrigger: ".animate-services",
+	duration: 1.2,
+	opacity: 0,
+	x: -200,
+})
